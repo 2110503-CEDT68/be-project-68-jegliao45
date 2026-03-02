c5da2e08f8b401f8744f97935ef1ebb1e5a8f7ac
Aomkrit <three010350@gmail.com>
2026-03-02 17:21:01 +0700

    create uml

--- routes/providers.js
+++ routes/providers.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { getProviders, createProvider} = require('../controllers/providers');
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/', getProviders);
 
-router.post('/', protect, createProvider);
+router.post('/', protect, authorize("admin"), createProvider);
 
 module.exports = router;
